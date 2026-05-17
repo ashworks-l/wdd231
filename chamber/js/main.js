71ddc21bc1b3f026d601a3915cd32b6b4046fa9a
Ashley L <alujanyez@byupathway.edu>
2026-05-18 00:35:53 +0200

    xdcbgfvnhjmklñ

---
+++ chamber/js/main.js
@@ -0,0 +1,13 @@
+document.getElementById("year").textContent =
+new Date().getFullYear();
+
+document.getElementById("lastModified").textContent =
+`Last Modification: ${document.lastModified}`;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});y 
