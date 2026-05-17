0ac9f972a0e6c2934813f3ec093f990ea89934a4
Ashley L <alujanyez@byupathway.edu>
2026-05-18 01:10:20 +0200

    sdfghjk

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,42 @@
+const membersContainer = document.querySelector("#members");
+
+async function getMembers() {
+const res = await fetch("data/members.json");
+const data = await res.json();
+displayMembers(data);
+}
+
+function displayMembers(members) {
+
+members.forEach(member => {
+
+const card = document.createElement("div");
+
+card.innerHTML = `
+<h3>${member.name}</h3>
+<p>${member.address}</p>
+<p>${member.phone}</p>
+<a href="${member.website}" target="_blank">Website</a>
+<img src="images/${member.image}" alt="${member.name}">
+`;
+
+membersContainer.appendChild(card);
+
+});
+
+}
+
+getMembers();
+
+/* view toggle */
+document.querySelector("#grid").addEventListener("click", () => {
+membersContainer.className = "grid";
+});
+
+document.querySelector("#list").addEventListener("click", () => {
+membersContainer.className = "list";
+});
+
+document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified;
+
