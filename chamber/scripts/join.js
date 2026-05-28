2aa1c38c3dd169e57ddb2636f68fb327bc2f342e
ashworks-l <valash1330@gmail.com>
2026-05-29 00:36:47 +0200

    qwerhm

---
+++ chamber/scripts/join.js
@@ -0,0 +1,35 @@
+```javascript
+const timestamp =
+document.getElementById("timestamp");
+
+timestamp.value =
+new Date().toLocaleString();
+
+/* YEAR */
+
+const year =
+document.getElementById("year");
+
+year.textContent =
+new Date().getFullYear();
+
+/* LAST MODIFIED */
+
+const modified =
+document.getElementById("lastModified");
+
+modified.textContent =
+document.lastModified;
+
+/* HAMBURGER */
+
+const menuButton =
+document.getElementById("menu-button");
+
+const navigation =
+document.getElementById("navigation");
+
+menuButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+});
+```
---
+++ chamber/scripts/thankyou.js
@@ -0,0 +1,27 @@
+const params =
+new URLSearchParams(window.location.search);
+
+document.getElementById("results").innerHTML = `
+
+<p><strong>First Name:</strong>
+${params.get("firstname")}</p>
+
+<p><strong>Last Name:</strong>
+${params.get("lastname")}</p>
+
+<p><strong>Email:</strong>
+${params.get("email")}</p>
+
+<p><strong>Phone:</strong>
+${params.get("phone")}</p>
+
+<p><strong>Organization:</strong>
+${params.get("organization")}</p>
+
+<p><strong>Date:</strong>
+${params.get("timestamp")}</p>
+
+`;
+
+document.getElementById("year").textContent =
+new Date().getFullYear();
