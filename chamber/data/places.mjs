99af516f7cbc60ea52a7b6e6cfdfbf30e72a63f3
ashworks-l <valash1330@gmail.com>
2026-06-04 22:58:13 +0200

    sedrftgyhui

---
+++ chamber/data/places.mjs
@@ -0,0 +1,50 @@
+export const places = [
+{
+name: "Plaza del Castillo",
+address: "Plaza del Castillo, Pamplona",
+image: "images/place1.jpg",
+description: "Historic square in the heart of Pamplona."
+},
+{
+name: "Pamplona Cathedral",
+address: "Calle Curia, Pamplona",
+image: "images/place2.jpg",
+description: "Gothic cathedral with museum and viewpoints."
+},
+{
+name: "Citadel of Pamplona",
+address: "Avenida del Ejército",
+image: "images/place3.jpg",
+description: "Historic fortress surrounded by green spaces."
+},
+{
+name: "Yamaguchi Park",
+address: "Pamplona",
+image: "images/place4.jpg",
+description: "Japanese-inspired park and gardens."
+},
+{
+name: "Planetarium",
+address: "Pamplona",
+image: "images/place5.jpg",
+description: "Science and astronomy exhibitions."
+},
+{
+name: "Town Hall",
+address: "Plaza Consistorial",
+image: "images/place6.jpg",
+description: "Famous building of the San Fermín festival."
+},
+{
+name: "Taconera Gardens",
+address: "Pamplona",
+image: "images/place7.jpg",
+description: "Historic gardens and walking paths."
+},
+{
+name: "Navarra Museum",
+address: "Pamplona",
+image: "images/place8.jpg",
+description: "Art and history museum of Navarra."
+}
+];
